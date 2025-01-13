fdb0ceca554a09f09976a589beb933dcb3c29825
SandhiyaKumar-18 <96000268+SandhiyaKumar-18@users.noreply.github.com>
2025-01-13 09:40:54 +0530

    update

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
 // import "./Nav.css";
 import "./TempNav.css"
 import Homee from "./Homee"
 import image from "../image/Vector.svg"
+import Skills from "./Skills";
 
 
 const Navbar = () => {
@@ -21,6 +23,7 @@ const Navbar = () => {
       
       <ul className={isMobile ? "nav-links-mobile" : "nav-links"} onClick={() => setIsMobile(false)}>
         <li className="hover-underline-animation"><a href ="/" style={{ textDecoration: 'none' }} className="aclass">Ho</a></li>
+        <NavLinkItem to="/Skills">Skills</NavLinkItem>
         <li className="hover-underline-animation"><a href ="/Homee" style={{ textDecoration: 'none' }} className="aclass">Home</a></li>
         <li className="hover-underline-animation"><a href="/Skills" style={{ textDecoration: 'none' }}className="aclass">Skills</a></li>
         <li className="hover-underline-animation"><a href="https://sandhiyakumar-18.github.io/Skills" style={{ textDecoration: 'none' }}className="aclass">Skills</a></li>
